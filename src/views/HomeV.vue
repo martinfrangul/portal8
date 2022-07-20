c32be5a13b4b4da4f8d438e36fd65ffbdfc915bc
Martín Frangul <postguerra@gmail.com>
2022-07-20 04:00:49 +0200

    Trabajando en Home

---
+++ src/views/HomeV.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Home from '../components/Home.vue'
+
+export default {
+  name: 'HomeV',
+  components: {
+    Home
+  }
+}
+</script>
